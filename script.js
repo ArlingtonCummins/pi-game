41d854c90570ddafe9b5a41b6c2d2e6b18d68659
Arlington Cummins <103346188+ArlingtonCummins@users.noreply.github.com>
2022-05-16 15:16:35 -0700

    Add Effects

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const piString = '1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679';
 const pi = Array.from(piString);
 
-const screen = document.getElementsByTagName('body')[0];
+let effectsEnabled = true;
+
 const piEl = document.getElementById('pi');
 const menu = document.getElementById('menu');
 const game = document.getElementById('game');
@@ -10,6 +11,7 @@ const phoneMessage = document.getElementById('phoneMessage');
 const start = document.getElementById('startButton');
 const restart = document.getElementById('restartButton');
 const menuButton = document.getElementById('menuButton');
+const effectButton = document.getElementById('effectButton')
 const score = document.getElementById('score');
 const highScore = document.getElementById('highScore');
 const missedDigit = document.getElementById('missedDigit');
@@ -18,52 +20,70 @@ let inGame = false;
 let currentDigit = 0;
 let sessionHighScore = 0;
 
-screen.addEventListener ('touchstart', () => {
-	menu.style.display = 'none';
-	game.style.display = 'none';
-	gameOver.style.display = 'none';
-	phoneMessage.style.display = 'grid';
+document.getElementsByTagName('body')[0].addEventListener('touchstart', () => {
+  menu.style.display = 'none';
+  game.style.display = 'none';
+  gameOver.style.display = 'none';
+  phoneMessage.style.display = 'grid';
 })
 
-start.addEventListener ('click', () => {
-	menu.style.display = 'none';
-	game.style.display = 'grid';
-	gameOver.style.display = 'none';
-	inGame = true;
+start.addEventListener('click', () => {
+  menu.style.display = 'none';
+  game.style.display = 'grid';
+  gameOver.style.display = 'none';
+  inGame = true;
 });
 
-restart.addEventListener ('click', () => {
-	menu.style.display = 'none';
-	game.style.display = 'grid';
-	gameOver.style.display = 'none';
-	inGame = true;
+restart.addEventListener('click', () => {
+  menu.style.display = 'none';
+  game.style.display = 'grid';
+  gameOver.style.display = 'none';
+  inGame = true;
 });
 
-menuButton.addEventListener ('click', () => {
-	menu.style.display = 'grid';
-	game.style.display = 'none';
-	gameOver.style.display = 'none';
+menuButton.addEventListener('click', () => {
+  menu.style.display = 'grid';
+  game.style.display = 'none';
+  gameOver.style.display = 'none';
 });
-	
-window.addEventListener ('keydown', (event) => {
-	if (inGame) {
-		if (event.key === pi[currentDigit].toString()) {
-			piEl.innerHTML = piEl.innerHTML + pi[currentDigit];
-			currentDigit++;
-		}
-		else {
-			score.innerHTML = 'Your Score was ' + currentDigit;
-			missedDigit.innerHTML = 'The digit you missed was ' + pi[currentDigit];
-			if (sessionHighScore < currentDigit) {
-				sessionHighScore = currentDigit;
-				highScore.innerHTML = 'Your high score for this session is ' + sessionHighScore;
-			}
-			currentDigit = 0;
-			piEl.innerHTML = '3.';
-			inGame = false;
-			menu.style.display = 'none';
-			game.style.display = 'none';
-			gameOver.style.display = 'grid';
-		}
-	}
+
+effectButton.addEventListener ('click', () => {
+  effectsEnabled = !effectsEnabled;
+  if (effectsEnabled) {
+    effectButton.innerHTML = 'Disable Effects';
+  }
+  else {
+    effectButton.innerHTML = 'Enable Effects';
+  }
+})
+
+window.addEventListener('keydown', (event) => {
+  if (inGame) {
+    if (event.key === pi[currentDigit].toString()) {
+      piEl.innerHTML = piEl.innerHTML + pi[currentDigit];
+      currentDigit++;
+    }
+    else {
+      score.innerHTML = 'Your Score was ' + currentDigit;
+      missedDigit.innerHTML = 'The digit you missed was ' + pi[currentDigit];
+      if (sessionHighScore < currentDigit) {
+        sessionHighScore = currentDigit;
+        highScore.innerHTML = 'Your high score for this session is ' + sessionHighScore;
+      }
+      currentDigit = 0;
+      piEl.innerHTML = '3.';
+      inGame = false;
+      menu.style.display = 'none';
+      game.style.display = 'none';
+      gameOver.style.display = 'grid';
+    }
+    
+    if (effectsEnabled) {
+      piEl.classList.add('anim');
+    }
+  }
+})
+
+piEl.addEventListener('animationend', () => {
+  piEl.classList.remove('anim');
 })
